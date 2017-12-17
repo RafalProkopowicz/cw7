060b9d8c7fd226f9c74562588f7c683b02662e51
Rafał Prokopowicz <rprokopowicz@linux-p110-10.inf.ug.edu.pl>
2017-12-17 13:50:29 +0100

    13:50

---
+++ zad12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+
+
+
+char napis2[10];
+
+char rekurencja(char* napis){
+  if (napis) {
+
+  }
+}
+
+int main(void){
+  char napis[10] = {'qwerty'};
+  rekurencja(napis);
+}
